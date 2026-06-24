a32e6919968985266c2c24632d8238af52c8e03a
gabou-code <campossequeiragabriel@gmail.com>
2026-06-23 18:19:01 -0600

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,21 @@ class Nodo:
         self.es_pregunta = es_pregunta
         self.izquierda = None #rama para el si
         self.derecha = None #rama para el no
+
+    def es_hoja(self):
+        return not self.es_pregunta
+    
+class ArbolDecision:
+
+    def __init__(self):
+        self.raiz = self.crear_arbol_defecto()
+        self.nodo_actual = self.raiz
+    
+    def crear_arbol_defecto(self):
+        #crea un arbol basico por defecto si no se carga ningún archivo
+        raiz = Nodo("¿Es un animal?", es_pregunta= True)
+        raiz.izquierda = Nodo("perro", es_pregunta= False)
+        raiz.derecho = Nodo("computadora", es_pregunta=False)
+        return raiz
+
+        
